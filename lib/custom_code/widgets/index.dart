81747e58c8ba7aea6a7e6424c0ab67c7f79bc085
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-07-29 14:00:29 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/index.dart
+++ lib/custom_code/widgets/index.dart
@@ -1,2 +1,4 @@
 export 'camera_custom_widget.dart' show CameraCustomWidget;
 export 'image_editor_widget.dart' show ImageEditorWidget;
+export 'media_preview_ios_widget.dart' show MediaPreviewIosWidget;
+export 'video_editor_widget.dart' show VideoEditorWidget;
