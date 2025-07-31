0e9f2548891e04715bd3254127d3d0e2b12ffc9d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-07-31 21:29:06 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/custom_code/actions/compress_image_for_upload_action.dart
@@ -0,0 +1,13 @@
+// Automatic FlutterFlow imports
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+/// Compress images after the Image Editor is done.
+Future<String> compressImageForUploadAction(String imagePath) async {
+  // Add your function code here!
+}
---
+++ lib/custom_code/actions/index.dart
@@ -0,0 +1,5 @@
+export 'compress_image_for_upload_action.dart'
+    show compressImageForUploadAction;
+export 'compress_video_for_upload_action.dart'
+    show compressVideoForUploadAction;
+export 'flip_video_action.dart' show flipVideoAction;
